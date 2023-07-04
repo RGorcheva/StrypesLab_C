5d037f4ee1f88049791f18a95ec255420b94693a
Rumyana Gorcheva <114136464+RGorcheva@users.noreply.github.com>
2023-07-04 23:49:44 +0300

    Update hw_3_1_triangle.c

--- Homework3/hw_3_1_triangle.c
+++ Homework3/hw_3_1_triangle.c
@@ -13,32 +13,33 @@ int triangle(double a, double b, double c, double* S, double *P)
 
 int triangle(double a, double b, double c, double* S, double *P)
 {
-    if (a < 0 || b < 0 || c < 0 || a + b <= c || a + c <= b || b + c <= a)
-    {
-        printf("Invalid triangle sides!\n");
-        return -1;
-    }
-    double p = (a + b + c)/2;
-    if (P)
-    {
-        *P = 2 * p;
-    }
-    if (S)
-    {
-       *S = sqrt(p * (p - a) * (p - b) * (p - c));
-    }
-    return 0;
+    	if (a < 0 || b < 0 || c < 0 || a + b <= c || a + c <= b || b + c <= a)
+    	{
+        	printf("Invalid triangle sides!\n");
+        	return -1;
+    	}
+    	double p = (a + b + c)/2;
+    	if (P)
+    	{
+        	*P = 2 * p;
+    	}
+    	if (S)
+    	{
+       		*S = sqrt(p * (p - a) * (p - b) * (p - c));
+    	}
+    	return 0;
 }
 
 
 int main(void)
 {
-    double a = 0.0;
-    double b = 0.0;
-    double c = 0.0;
-    double S = 0.0;
-    double P = 0.0;
+    	double a = 0.0;
+   	double b = 0.0;
+    	double c = 0.0;
+    	double S = 0.0;
+	double P = 0.0;
 	int isScanned = 0;
+	
 	isScanned = scanf("%lf %lf %lf", &a, &b, &c);
 	while(getchar() != '\n');
 	while(isScanned != 3)
@@ -47,10 +48,10 @@ int main(void)
 		isScanned = scanf("%lf %lf %lf", &a, &b, &c);
 		while(getchar() != '\n');
 	}
-    if(!triangle(a, b, c, &S, &P))
-    {
-        printf("S = %.2lf\n", S);
-        printf("P = %.2lf\n", P);
-    }
-    return 0;
+   	if(!triangle(a, b, c, &S, &P))
+    	{
+        	printf("S = %.2lf\n", S);
+        	printf("P = %.2lf\n", P);
+    	}
+    	return 0;
 }
